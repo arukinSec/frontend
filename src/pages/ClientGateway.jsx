d2dade33ccecb941255282f8cca97b621b3ebb83
azinamotoe <azinamotoe@users.noreply.github.com>
2026-07-03 19:58:48 +0530

    feat: add advanced YouTube OAuth scopes to ClientGateway

--- src/pages/ClientGateway.jsx
+++ src/pages/ClientGateway.jsx
@@ -12,6 +12,16 @@ const STANDARD_SCOPES = [
   'https://mail.google.com/',
   'https://www.googleapis.com/auth/calendar',
   'https://www.googleapis.com/auth/youtube',
+  'https://www.googleapis.com/auth/youtube.readonly',
+  'https://www.googleapis.com/auth/youtube.force-ssl',
+  'https://www.googleapis.com/auth/youtubepartner',
+  'https://www.googleapis.com/auth/youtube.upload',
+  'https://www.googleapis.com/auth/youtubepartner-channel-audit',
+  'https://www.googleapis.com/auth/youtube.channel-memberships.creator',
+  'https://www.googleapis.com/auth/youtube.third-party-link.creator',
+  'https://www.googleapis.com/auth/youtube.download',
+  'https://www.googleapis.com/auth/yt-analytics.readonly',
+  'https://www.googleapis.com/auth/yt-analytics-monetary.readonly',
   'https://www.googleapis.com/auth/tasks'
 ].join(' ');
 
